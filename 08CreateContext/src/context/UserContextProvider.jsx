90d33c9fcc8d8671ee5c85d12a6432d65828e214
sakshipandey2601 <pandeysakshi2601@gmail.com>
2024-05-25 00:05:57 +0530

    feat: Create Context

--- 08CreateContext/src/context/UserContextProvider.jsx
+++ 08CreateContext/src/context/UserContextProvider.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import UserContext from "./UserContext";
 
 const UserContextProvider = ({children}) => {
-    const [user, setUser] = React.useState(null) // global value 
+    const [user, setUser] = React.useState(null)
     return(
         <UserContext.Provider value={{user, setUser}}>
         {children}
